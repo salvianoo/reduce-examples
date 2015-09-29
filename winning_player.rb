c005027548a05de971aa60cc03d4faf1019c4e5c
Salviano Ludgerio <salviano.lfg@gmail.com>
2015-09-29 13:39:29 -0300

    Code cleaner

--- winning_player.rb
+++ winning_player.rb
@@ -8,9 +8,8 @@ class Player
   end
 
   def self.winner_player(all_players)
-    all_players
-      .map { |player| player.score }
-      .reduce { |max_score, score| score > max_score ? score : max_score }
+    compare = ->(p1, p2) { p1.score > p2.score ? p1 : p2 }
+    all_players.reduce(&compare)
   end
 
   def self.all_players
@@ -27,4 +26,4 @@ Player.add_player Player.new('Steve', 8.2)
 Player.add_player Player.new('Bruce', 9.2)
 
 winner = Player.winner_player(Player.all_players)
-puts "Best score: #{winner}"
+puts "Best score: #{winner.score}"
